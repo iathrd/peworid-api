c4f89c0795193752b6dc94f85b953f3337e52752
iqbal <iqbalathoridd@gmail.com>
2021-09-13 14:58:43 +0700

    initial auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [AuthModule],
+  controllers: [AuthController],
   providers: [],
 })
 export class AppModule {}
