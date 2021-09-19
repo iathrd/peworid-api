383485d2e16ffab5066453ab11dfaf319bcc905a
iqbal <iqbalathoridd@gmail.com>
2021-09-19 16:43:36 +0700

    create user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { configValidationSchema } from './config.schema';
 
@@ -27,7 +26,5 @@ import { configValidationSchema } from './config.schema';
     }),
     AuthModule,
   ],
-  controllers: [AuthController],
-  providers: [],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
