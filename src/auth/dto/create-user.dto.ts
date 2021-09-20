070c0a880b4240e70ff32377e4bbc21373af4c87
iqbal <iqbalathoridd@gmail.com>
2021-09-20 17:30:20 +0700

    added validation

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,7 +1,27 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
   id: string;
+
+  @IsNotEmpty()
   username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber('ID')
   phoneNumber: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)
   password: string;
 }
