5b7199b572d110df0fd462d0557b2abc2d5922f3
iqbal <iqbalathoridd@gmail.com>
2021-09-20 18:26:29 +0700

    handle error if already exist

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,10 @@ import { EntityRepository, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 import * as argon from 'argon2';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -11,8 +15,16 @@ export class UserRepository extends Repository<User> {
       ...cretaeUserDto,
       password: hashedPassword,
     });
-    await this.create(user);
 
-    return user;
+    try {
+      await this.save(user);
+      return user;
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException(error.detail);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
