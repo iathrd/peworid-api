8f4a0b4fc796aa7fd4aa6daf70673c6e92216a03
iqbal <iqbalathoridd@gmail.com>
2021-09-22 10:35:08 +0700

    create signin controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { SignInUserDto } from './dto/signin-user.dto';
 import { User } from './user.entity';
 
 @Controller('auth')
@@ -10,5 +11,10 @@ export class AuthController {
   @Post('/signup')
   createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.authService.createUser(createUserDto);
+  }
+
+  @Post('/signin')
+  signIn(@Body() signInUserDto: SignInUserDto): Promise<string> {
+    return this.authService.signIn(signInUserDto);
   }
 }
